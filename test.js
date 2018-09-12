63c4f50874feaf74a7627a3409be619798928e5d
Your Name <kasra.shahram@gmail.com>
2018-09-12 11:20:57 +0200

    ok

--- test.js
+++ test.js
@@ -1,12 +1,12 @@
 import commander_wraper from './index'
-import program from 'commander'
-import _ from 'lodash'
-let names         =  ['competition','club','fields','staff'];
-let options       =  [['a','agenda'],['r','results'],['c','calendar'],['o','ranking']]
+import program          from 'commander'
+import _                from 'lodash'
+
+let names         =  ['cmd1','cmd2','cmd3','cmd4'];
+let options       =  [['a','opt_a'],['b','opt_b'],['c','opt_c'],['d','opt_d']]
 let configuration =  {names,version:'0.0.1',options}
 
-let f = (x,y,z)=>{
-   z.forEach(x=>console.log(x)) 
-}
+let f = (x,y,z)=>{z.forEach(x=>console.log(x)) }
 let g =  _.curry(f)(10,20)
+
 commander_wraper.configure_commander(program,configuration,g)
