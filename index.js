ba7c1aa22170c3b50708893c630457caf76a76d8
Your Name <kasra.shahram@gmail.com>
2018-09-14 16:34:31 +0200

    ok

--- index.js
+++ index.js
@@ -4,14 +4,13 @@ import  _        from 'lodash'*/
 let program = require('commander')
 let _       = require('lodash')
 
-let opt =(sign,name)=> [`-${sign} --${name}`,`get ${name}`];
-let op = (parent,...args)=> parent.option(...args)
-let command = (program,name,options,action)=>{ 
-   let x =   program.command(`${name} <args...> `)
-   let q = options.map(x=>opt(...x))
+let opt = (sign,name)=> [`-${sign} --${name}`,`get ${name}`];
+let op  = (parent,...args)=> parent.option(...args)
+let command = (program,cmd_name,options,action)=>{ 
+    let x =   program.command(`${cmd_name} <args...> `)
+    let q =   options.map(x=>opt(...x))
     x = q.reduce((previous,current)=> op(previous,...current),x)
     let options_name = options.map(x=>x[1])
-    let options_key  = options.map(x=>x[1])
     x.action(function (data, cmd) {
           data.forEach(x => {
          let keys_cmd = _.keys(cmd).filter(x=>_.includes(options_name,x))
@@ -19,16 +18,17 @@ let command = (program,name,options,action)=>{
           for(let i = 0 ; i < keys_cmd.length ; i++){                  
              flags += _.padEnd(keys_cmd[i] ,15)                           
            }
-          console.log(`${_.padEnd(name,15)} ${_.padEnd(x,10)}=> ${flags} `); 
+          console.log(`${_.padEnd(cmd._name,15)} ${_.padEnd(x,10)}=> ${flags} `); 
         });
         action(data,cmd)
     });
 }
-let configure_commander = (program,{version,names,options},action)=>{
+let configure_commander = (program,version,...configurations)=>{    
     program.version(version)
-    names.forEach(name=>command(program,name,options,action))
+    configurations.forEach(({cmd,options,action})=> command(program,cmd,options,action))
     program.parse(process.argv)
 }
+
 
 module.exports = {command,configure_commander}
 //export {command,configure_commander}
--- test.js
+++ test.js
@@ -1,12 +1,14 @@
 import commander_wraper from './index'
 import program          from 'commander'
 import _                from 'lodash'
+let options0      =  [['a','opt_a'],['b','opt_b'],['c','opt_c'],['d','opt_d']]
+let options1      =  [['e','opt_e'],['f','opt_f']]
+let f0      = (x,y,z)=>{z.forEach(x=>console.log(parseInt(x) +  1)) }
+let f1      = (x,y,z)=>{z.forEach(x=>console.log(x)) }
+let g0      =  _.curry(f0)(10,20)
+let g1      =  _.curry(f1)(10,20)
+let c0      = {cmd:'cmd1',options:options0,action:g0}
+let c1      = {cmd:'cmd2',options:options1,action:g1}
+let configs = [c0,c1]
+commander_wraper.configure_commander(program,'0.0.1',...configs)
 
-let names         =  ['cmd1','cmd2','cmd3','cmd4'];
-let options       =  [['a','opt_a'],['b','opt_b'],['c','opt_c'],['d','opt_d']]
-let configuration =  {names,version:'0.0.1',options}
-
-let f = (x,y,z)=>{z.forEach(x=>console.log(x)) }
-let g =  _.curry(f)(10,20)
-
-commander_wraper.configure_commander(program,configuration,g)
