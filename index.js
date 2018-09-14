7a07d4c01780a85aba39aef3dc36550ac4ad4881
Your Name <kasra.shahram@gmail.com>
2018-09-14 16:48:49 +0200

    ok

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 /*import  program  from "commander"
 import  _        from 'lodash'*/
-
 let program = require('commander')
 let _       = require('lodash')
 
 let opt = (sign,name)=> [`-${sign} --${name}`,`get ${name}`];
 let op  = (parent,...args)=> parent.option(...args)
+
 let command = (program,cmd_name,options,action)=>{ 
     let x =   program.command(`${cmd_name} <args...> `)
     let q =   options.map(x=>opt(...x))
@@ -13,22 +13,20 @@ let command = (program,cmd_name,options,action)=>{
     let options_name = options.map(x=>x[1])
     x.action(function (data, cmd) {
           data.forEach(x => {
-         let keys_cmd = _.keys(cmd).filter(x=>_.includes(options_name,x))
-          let flags   = '';
-          for(let i = 0 ; i < keys_cmd.length ; i++){                  
-             flags += _.padEnd(keys_cmd[i] ,15)                           
-           }
-          console.log(`${_.padEnd(cmd._name,15)} ${_.padEnd(x,10)}=> ${flags} `); 
+          let keys_cmd = _.keys(cmd).filter(x=>_.includes(options_name,x))   
+          let r        = _.reduce(keys_cmd,(previous,current,index)=>`${previous}${_.padEnd(current,15)}`,'')
+          let message  = `${_.padEnd(cmd._name,15)} ${_.padEnd(x,10)}=> ${r} `
+          console.log(message); 
         });
         action(data,cmd)
     });
 }
-let configure_commander = (program,version,...configurations)=>{    
+
+let configure_commander = (version,...configurations)=>{    
     program.version(version)
     configurations.forEach(({cmd,options,action})=> command(program,cmd,options,action))
     program.parse(process.argv)
 }
-
 
 module.exports = {command,configure_commander}
 //export {command,configure_commander}
--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import commander_wraper from './index'
-import program          from 'commander'
+
 import _                from 'lodash'
 let options0      =  [['a','opt_a'],['b','opt_b'],['c','opt_c'],['d','opt_d']]
 let options1      =  [['e','opt_e'],['f','opt_f']]
@@ -10,5 +10,5 @@ let g1      =  _.curry(f1)(10,20)
 let c0      = {cmd:'cmd1',options:options0,action:g0}
 let c1      = {cmd:'cmd2',options:options1,action:g1}
 let configs = [c0,c1]
-commander_wraper.configure_commander(program,'0.0.1',...configs)
+commander_wraper.configure_commander('0.0.1',...configs)
 
